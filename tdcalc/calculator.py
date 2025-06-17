acf4dbef7fe8aed28d594014e3f29d51a1bc67f5
Connor <cjflowers@pm.me>
2025-06-17 15:38:08 +1000

    calculate simple interest

--- tdcalc/calculator.py
+++ tdcalc/calculator.py
@@ -1,2 +1,2 @@
-def calculate_compound_interest():
-    ...
+def calculate_simple_interest(deposit: float, interest_rate: float, years: int) -> float:
+    return deposit * interest_rate * years
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,6 +1,6 @@
-from tdcalc.calculator import calculate_compound_interest
+from tdcalc.calculator import calculate_simple_interest
 
-def test_calculate_compound_interest():
-    result = calculate_compound_interest()
+def test_calculate_simple_interest():
+    result = calculate_simple_interest(10_000, 0.011, 3)
 
-    assert result == 330
+    assert result == 330.0
